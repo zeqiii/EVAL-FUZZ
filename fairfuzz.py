f857bf1bda9bec42aecfa318dd30a60bc4aeb548
zeqihao <zeqihao@gmail.com>
2021-04-13 17:05:28 +0800

    update
    add fairfuzz

--- fairfuzz.py
+++ fairfuzz.py
@@ -8,16 +8,16 @@ class Runner_fairfuzz(Runner):
         Runner.__init__(self, fuzzer_name, target_path, seed_dir, round_num)
 
     def compile(self):
-        self.target_binary = self.do_compile(Global.SCRIPT, self.target_path, args="TORTOISE", binary_postfix=Global.BINARY_POSTFIX)
+        self.target_binary = self.do_compile(Global.SCRIPT, self.target_path, args="FAIRFUZZ", binary_postfix=Global.BINARY_POSTFIX)
         
-    def start_fuzz_tortoise(self, extra_args):
+    def start_fuzz_fairfuzz(self, extra_args):
         self.finish = False
         self.keywords.append("afl-fuzz")
         run_cmd = self.gen_run_cmd(target_binary=self.target_binary, input_file="@@")
-        cmd = "%s -i %s -o %s -s -m none -- %s 1>/dev/null 2>&1" %(Global.PATH[self.fuzzer_name], self.seed_dir, self.one_output_dir, run_cmd)
+        cmd = "%s -i %s -o %s -q 1 -m none -- %s 1>/dev/null 2>&1" %(Global.PATH[self.fuzzer_name], self.seed_dir, self.one_output_dir, run_cmd)
         self.process.append(subprocess.Popen(cmd, shell=True))
 
     def start_fuzz(self, extra_args=[]):
         crash_dir = os.path.join(one_output_dir, "crashes")
-        self.start_fuzz_tortoise(extra_args)
+        self.start_fuzz_fairfuzz(extra_args)
 
--- scripts/build_target_afl_ebench.sh
+++ scripts/build_target_afl_ebench.sh
@@ -10,4 +10,6 @@ elif [ $mode == "ORIG" ]; then
     CC=gcc make
 elif [ $mode == "TORTOISE" ]; then
     CC=/home/varas/workspace/TortoiseFuzz/bb_metric/afl-clang-fast AFL_DONT_OPTIMIZE=1 AFL_USE_ASAN=1 make
+elif [ $mode == "FAIRFUZZ" ]; then
+	CC=/home/varas/workspace/afl-rb/afl-clang AFL_DONT_OPTIMIZE=1 AFL_USE_ASAN=1 make
 fi
