283e35828f927323aed4754ec439304166552bac
zeqihao <zeqihao@gmail.com>
2021-04-14 21:49:33 +0800

    update
    1

---
+++ honggfuzz.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import Global, os
+from fuzzing import *
+
+class Runner_honggfuzz(Runner):
+    def __init__(self, fuzzer_name, target_path, seed_dir, round_num):
+        Runner.__init__(self, fuzzer_name, target_path, seed_dir, round_num)
+
+    def compile(self):
+        self.target_binary = self.do_compile(Global.SCRIPT, self.target_path, args="HONGGFUZZ", binary_postfix=Global.BINARY_POSTFIX)
+        
+    def start_fuzz_fairfuzz(self, extra_args):
+        self.finish = False
+        self.keywords.append("afl-fuzz")
+        run_cmd = self.gen_run_cmd(target_binary=self.target_binary, input_file="@@")
+        cmd = "%s -i %s -o %s -q 1 -m none -- %s 1>/dev/null 2>&1" %(Global.PATH[self.fuzzer_name], self.seed_dir, self.one_output_dir, run_cmd)
+        self.process.append(subprocess.Popen(cmd, shell=True))
+
+    def start_fuzz(self, extra_args=[]):
+        self.crash_dir = os.path.join(self.one_output_dir, "crashes")
+        self.start_fuzz_fairfuzz(extra_args)
--- scripts/build_target_afl_ebench.sh
+++ scripts/build_target_afl_ebench.sh
@@ -12,4 +12,6 @@ elif [ $mode == "TORTOISE" ]; then
     CC=/home/varas/workspace/TortoiseFuzz/bb_metric/afl-clang-fast AFL_DONT_OPTIMIZE=1 AFL_USE_ASAN=1 make
 elif [ $mode == "FAIRFUZZ" ]; then
 	CC=/home/varas/workspace/afl-rb/afl-clang AFL_DONT_OPTIMIZE=1 AFL_USE_ASAN=1 make
+elif [ $mode == "HONGGFUZZ" ]; then
+	CC=/home/varas/workspace/honggfuzz/hfuzz_cc/hfuzz-clang make
 fi
